0e07a880487794d3d70f214739d5329de9b17010
Anton Selkin <anton.selkin@gmail.com>
2021-09-29 18:18:50 +0300

    add same UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,87 @@
-import 'package:flutter/material.dart';
-import 'package:radioapp/utils/ai_util.dart';
-import 'package:velocity_x/velocity_x.dart';
+import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
+import 'package:radioapp/model/radio.dart';
+import "package:radioapp/utils/ai_util.dart";
+import "package:velocity_x/velocity_x.dart";
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  late List<MyRadio> radios;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchRadios();
+    
+  }
+
+  fetchRadios() async{
+    final radioJson = await rootBundle.loadString("assets/radio.json");
+    radios = MyRadioList.fromJson(radioJson).radios;
+    print(radios);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: AIColors.primaryColor1,
-    );
+    return Scaffold(
+      drawer: const Drawer(),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          VxAnimatedBox()
+              .size(context.screenWidth, context.screenHeight)
+              .withGradient(
+                LinearGradient(colors: [
+                  AIColors.primaryColor1,
+                  AIColors.primaryColor2,
+                ],begin: Alignment.topLeft, end: Alignment.bottomRight),
+              )
+              .make(),
+              AppBar(
+                title: "AI Radio".text.xl4.bold.white.make().shimmer(
+                  primaryColor:  Vx.purple300, secondaryColor: Colors.white),
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                centerTitle:  true,
+              ).h(100.0).p16(),
+              VxSwiper.builder(
+                itemCount: radios.length,
+                aspectRatio: 1.0,
+                enlargeCenterPage: true,
+                itemBuilder: (context, index) {
+                  final rad = radios[index];
+
+                  return VxBox(
+                    child: ZStack([
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: VStack([
+                          rad.name.text.xl3.white.bold.make(),
+                          5.heightBox,
+                          rad.tagline.text.sm.white.semiBold.make(),
+                        ]),
+                      )
+                    ])
+                  )
+                  .bgImage(DecorationImage(
+                    image: NetworkImage(rad.image),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.darken)),
+                    )
+                  .border(width: 5.0)  
+                  .withRounded(value: 60.0)  
+                  .make()
+                  .p16()
+                  .centered();
+                },
+                ), 
+          ],
+        ),
+      );
   }
 }
